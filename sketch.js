a649e724543ecc0b51ee2e7684402b2d611fec09
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-14 15:18:44 -0400

    so I'm totally going against his specifics

--- sketch.js
+++ sketch.js
@@ -1,53 +1,77 @@
 /// <reference path="./node_modules/@types/p5/global.d.ts" />
 
-class Ember {
-    constructor(x,y,r,g,b) {
-       this.x = x;
-       this.y = y;
-       this.dx = Math.random() * 200 - 100;
-       this.dy = Math.random() * 200 - 160;
-       this.r = r;
-       this.g = g;
-       this.b = b;
-       this.lifespan = Math.random() * 500;
-    }
-
-    render() {
-        fill(this.r,this.g,this.b,Math.log(this.lifespan / 20) * 100 * noise(frameCount,this.x + this.y + this.dy + this.dx));
-        noStroke();
-        ellipseMode(CENTER);
-        ellipse(this.x,this.y,10,10);
-
-        this.x += this.dx / frameRate();
-        this.y += this.dy  / frameRate();
-
-        this.dy += 50 / frameRate();
-
-        this.lifespan--;
-
-        return this.lifespan > 0;
-    }
-}
-
-var embers = [];
-
+const FRAMERATE = 60;
+const CART_VELOCITY = 100;
+// const PARTICLE_GRAVITY = 3;
 const RAIL_DENSITY = 40;
 const RAIL_OFFSET = 10;
 const RAIL_WIDTH = 100;
 
+class RockWall {
+    constructor(x,min,width,distCoeff,shade) {
+        this.x = x;
+        this.width = width;
+        this.distCoeff = distCoeff;
+        this.y = 0;
+        this.shade = shade;
+        this.min = min;
+
+    }
+
+    render() {
+        this.y += CART_VELOCITY * this.distCoeff / FRAMERATE;
+        fill(this.shade);
+        beginShape();
+        vertex(this.x,0);
+        for(var y = 0; y < height; y+=6) {
+            vertex(this.x + noise(this.x,(y + this.y) / 60) * (this.width - this.min) + this.min,y)
+        }
+        vertex(this.x,height);
+        endShape(CLOSE);
+    }
+}
+
+
+var embers = {
+    background: [],
+    foreground: []
+}
+
+const rocks = [];
+
 function setup() {
     createCanvas(windowWidth - 20, windowHeight - 20);
+    frameRate(FRAMERATE);
+    const min = 0;
+    const max = 100;
+    const count = 40;
+    for(var i = 0; i < count; i++) {
+        rocks.push(
+            new RockWall(
+                width / 2 - 20 - (width / 70) * i ,
+                min,
+                max,
+                i / 2,
+                (i / count)* 190
+            )
+        );
+        rocks.push(
+            new RockWall(
+                width / 2 + 20 + (width / 70) * i ,
+                -min,
+                -max,
+                i / 2,
+                (i / count)* 255
+            )
+        )
+    }
 }
 
 function draw() {
     background(20);
-
-    if(embers.length < 500) {
-        embers.push(new Ember(width/2,height/2,random(170,255),random(0,50),random(0,50)));
-    }
-
+    rocks.forEach(wall => wall.render());
+    randomSeed(frameCount);
     renderForeground();
-    renderEmbers();
 }
 
 function renderForeground() {
@@ -62,8 +86,6 @@ function renderForeground() {
         fill((1 - i / RAIL_DENSITY) * (1 - i / RAIL_DENSITY) * 100, (1 - i / RAIL_DENSITY) * (1 - i / RAIL_DENSITY) * 60, (1 - i / RAIL_DENSITY) * (1 - i / RAIL_DENSITY) * 17);
         rect(width / 2, height, width - RAIL_WIDTH - RAIL_OFFSET * 2, 200 * (i / RAIL_DENSITY));
     }
-}
 
-function renderEmbers() {
-    embers = embers.filter((ember) => ember.render());
+
 }
