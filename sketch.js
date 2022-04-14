668dd7f65ca054861dd164e3f6670313b85b0ceb
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-14 15:25:35 -0400

    Added comments

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ const RAIL_OFFSET = 10;
 const RAIL_WIDTH = 100;
 
 class RockWall {
+    //Constructor for a single column of rocks
     constructor(x,min,width,distCoeff,shade) {
         this.x = x;
         this.width = width;
@@ -18,22 +19,23 @@ class RockWall {
     }
 
     render() {
+        //Increments y by the cart velocity based on distance
         this.y += CART_VELOCITY * this.distCoeff / FRAMERATE;
+        //Fills with the shade specified
         fill(this.shade);
+
+        //Begins shape
         beginShape();
         vertex(this.x,0);
+
         for(var y = 0; y < height; y+=6) {
+            //Renders based on noise and positional arguments
             vertex(this.x + noise(this.x,(y + this.y) / 60) * (this.width - this.min) + this.min,y)
         }
+        //closes
         vertex(this.x,height);
         endShape(CLOSE);
     }
-}
-
-
-var embers = {
-    background: [],
-    foreground: []
 }
 
 const rocks = [];
@@ -41,6 +43,8 @@ const rocks = [];
 function setup() {
     createCanvas(windowWidth - 20, windowHeight - 20);
     frameRate(FRAMERATE);
+
+    //Creates the rock walls iteratively
     const min = 0;
     const max = 100;
     const count = 40;
@@ -67,8 +71,9 @@ function setup() {
 }
 
 function draw() {
+    //Renders for each rock
     rocks.forEach(wall => wall.render());
-    randomSeed(frameCount);
+    //Render the foreground
     renderForeground();
 }
 
@@ -76,11 +81,12 @@ function renderForeground() {
     noStroke();
     rectMode(CENTER);
     for (var i = RAIL_DENSITY; i >= 0; i--) {
+        //Side rails
         fill((1 - i / RAIL_DENSITY) * (1 - i / RAIL_DENSITY) * 140);
         rect(RAIL_OFFSET + RAIL_WIDTH / 2, height / 2, (i / RAIL_DENSITY) * RAIL_WIDTH, height);
         rect(width - (RAIL_OFFSET + RAIL_WIDTH / 2), height / 2, (i / RAIL_DENSITY) * RAIL_WIDTH, height);
 
-
+        //Bottom rail
         fill((1 - i / RAIL_DENSITY) * (1 - i / RAIL_DENSITY) * 100, (1 - i / RAIL_DENSITY) * (1 - i / RAIL_DENSITY) * 60, (1 - i / RAIL_DENSITY) * (1 - i / RAIL_DENSITY) * 17);
         rect(width / 2, height, width - RAIL_WIDTH - RAIL_OFFSET * 2, 200 * (i / RAIL_DENSITY));
     }
